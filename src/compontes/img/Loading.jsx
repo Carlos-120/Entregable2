753b15bdcfa97351c6c61530010c2d150ce01ab2
Carlos.-120 <cpalate18@gmail.com>
2022-11-30 15:25:35 -0500

    cambio de estilos app del clima

---
+++ src/compontes/img/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='container'>
+      <div className='cicle'></div>
+      <div className='cicle'></div>
+      <div className='cicle'></div>
+      <span className='Loading'>Loading ... </span>
+    </div>
+  )
+}
+
+export default Loading
