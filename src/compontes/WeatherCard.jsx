935b032f8e0f8124e6889ca82d44224ce3e99ffe
Carlos.-120 <cpalate18@gmail.com>
2022-12-01 13:34:26 -0500

    Aplicando grid

--- src/compontes/WeatherCard.jsx
+++ src/compontes/WeatherCard.jsx
@@ -7,22 +7,22 @@ const WeatherCard = ({ weather, temperature, isCelsius, changeUnitTemperature })
   return (
 
     <article className='time'>
-      <span id='span1'></span>
-      <span id='span1'></span>
-      <span id='span1'></span>
-      <span id='span1'></span>
+
       <h1>Weather App</h1>
       <h3>{`${weather.name} ${weather.sys.country}`}</h3>
-      <section>
-        <div>
+      <section className='grid'>
+        <div className='conten'>
           <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
         </div>
-        <ul>
-          <li>{weather.weather[0].description}</li>
-          <li>Wind speed:{weather.wind.speed} m/s</li>
-          <li>Clouds:{weather.clouds.all} %</li>
-          <li>Pressure:{weather.main.pressure} hPa</li>
-        </ul>
+        <div className='conten1'>
+          <ul>
+            <li className='Mayusculas'>{weather.weather[0].description}</li>
+            <li>Wind speed:{weather.wind.speed} m/s</li>
+            <li>Clouds:{weather.clouds.all} %</li>
+            <li>Pressure:{weather.main.pressure} hPa</li>
+          </ul>
+        </div>
+
       </section>
       <p>{isCelsius ? `${temperature.celsius} °C ` : `${temperature.fahrenheit} °F`} K</p>
       <button className='boton' onClick={changeUnitTemperature}> <span>Degrees °F/°C</span> </button>
